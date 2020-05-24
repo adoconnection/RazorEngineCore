6aa073c3bee90c51f13e30faa702c901d620d2bd
William David Cossey <wdcossey@github.com>
2020-05-24 14:21:55 +0100

    Added Async() methods. Updated to Load/Save File to use FileStream as it supports asynchronous IO. Simply duplicated synchronous Unit Tests into asynchronous ones.

--- RazorEngineCore/RazorEngineTemplateBase.cs
+++ RazorEngineCore/RazorEngineTemplateBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Dynamic;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
 
 namespace RazorEngineCore
@@ -42,8 +41,9 @@ namespace RazorEngineCore
             this.attributeSuffix = null;
         }
 
-        public async virtual Task ExecuteAsync()
+        public virtual Task ExecuteAsync()
         {
+            return Task.CompletedTask;
         }
 
         public string Result()
--- RazorEngineCore/RazorEngineTemplateBaseT.cs
+++ RazorEngineCore/RazorEngineTemplateBaseT.cs
@@ -3,6 +3,5 @@
     public abstract class RazorEngineTemplateBase<T> : RazorEngineTemplateBase
     {
         public new T Model { get; set; }
-
     }
 }   
