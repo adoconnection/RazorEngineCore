7975beb8035d3ef8b0cea79142d56e597510b8c5
Dag Håkon Baardsen <dagh@baardsen.org>
2020-07-22 13:04:32 +0200

    Removed explicit loading of netstandard on Linux/OSX only

--- RazorEngineCore/RazorEngineCompilationOptions.cs
+++ RazorEngineCore/RazorEngineCompilationOptions.cs
@@ -1,20 +1,38 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Microsoft.CodeAnalysis;
 
 namespace RazorEngineCore
 {
     public class RazorEngineCompilationOptions
     {
-        public HashSet<Assembly> ReferencedAssemblies { get; set; } = new HashSet<Assembly>()
-        {
-            typeof(object).Assembly,
-            Assembly.Load(new AssemblyName("Microsoft.CSharp")),
-            typeof(RazorEngineTemplateBase).Assembly,
-            Assembly.Load(new AssemblyName("System.Runtime")),
-            Assembly.Load(new AssemblyName("System.Linq")),
-            Assembly.Load(new AssemblyName("System.Linq.Expressions"))
-        };
+        private HashSet<Assembly> _referencedAssemblies;
+        public HashSet<Assembly> ReferencedAssemblies {
+            get
+            {
+                if (this._referencedAssemblies != null)
+                    return this._referencedAssemblies;
+
+                this._referencedAssemblies = new HashSet<Assembly>()
+                {
+                    typeof(object).Assembly,
+                    Assembly.Load(new AssemblyName("Microsoft.CSharp")),
+                    typeof(RazorEngineTemplateBase).Assembly,
+                    Assembly.Load(new AssemblyName("System.Runtime")),
+                    Assembly.Load(new AssemblyName("System.Linq")),
+                    Assembly.Load(new AssemblyName("System.Linq.Expressions"))
+                };
+
+                // Loading netstandard explicitly causes runtime error on Linux/OSX
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    this._referencedAssemblies.Add(Assembly.Load(new AssemblyName("netstandard")));
+                }
+
+                return this._referencedAssemblies;
+            }
+        } 
 
         public HashSet<MetadataReference> MetadataReferences { get; set; } = new HashSet<MetadataReference>();
 
