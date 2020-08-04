833390fad69f460dd41e9ebb16c1d370e0208003
Alexnader Selishchev <ADOConnection@gmail.com>
2020-08-04 11:58:00 +0300

    formatting

--- RazorEngineCore/RazorEngineCompilationOptions.cs
+++ RazorEngineCore/RazorEngineCompilationOptions.cs
@@ -7,35 +7,17 @@ namespace RazorEngineCore
 {
     public class RazorEngineCompilationOptions
     {
-        private HashSet<Assembly> _referencedAssemblies;
-        public HashSet<Assembly> ReferencedAssemblies {
-            get
-            {
-                if (this._referencedAssemblies != null)
-                    return this._referencedAssemblies;
-
-                this._referencedAssemblies = new HashSet<Assembly>()
-                {
-                    typeof(object).Assembly,
-                    Assembly.Load(new AssemblyName("Microsoft.CSharp")),
-                    typeof(RazorEngineTemplateBase).Assembly,
-                    Assembly.Load(new AssemblyName("System.Runtime")),
-                    Assembly.Load(new AssemblyName("System.Linq")),
-                    Assembly.Load(new AssemblyName("System.Linq.Expressions"))
-                };
-
-                // Loading netstandard explicitly causes runtime error on Linux/OSX
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    this._referencedAssemblies.Add(Assembly.Load(new AssemblyName("netstandard")));
-                }
-
-                return this._referencedAssemblies;
-            }
-        } 
+        public HashSet<Assembly> ReferencedAssemblies { get; set; } = new HashSet<Assembly>()
+        {
+            typeof(object).Assembly,
+            Assembly.Load(new AssemblyName("Microsoft.CSharp")),
+            typeof(RazorEngineTemplateBase).Assembly,
+            Assembly.Load(new AssemblyName("System.Runtime")),
+            Assembly.Load(new AssemblyName("System.Linq")),
+            Assembly.Load(new AssemblyName("System.Linq.Expressions"))
+        };
 
         public HashSet<MetadataReference> MetadataReferences { get; set; } = new HashSet<MetadataReference>();
-
         public string TemplateNamespace { get; set; } = "TemplateNamespace";
         public string Inherits { get; set; } = "RazorEngineCore.RazorEngineTemplateBase";
 
@@ -43,5 +25,14 @@ namespace RazorEngineCore
         {
             "System.Linq"
         };
+
+        public RazorEngineCompilationOptions()
+        {
+            // Loading netstandard explicitly causes runtime error on Linux/OSX
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                this.ReferencedAssemblies.Add(Assembly.Load(new AssemblyName("netstandard")));
+            }
+        }
     }
 }
