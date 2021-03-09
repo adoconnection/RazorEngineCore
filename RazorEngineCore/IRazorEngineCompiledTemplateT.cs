85932bfd751b13d1d3fe076a0d915be4a9b5abb4
Alexnader Selishchev <ADOConnection@gmail.com>
2021-03-09 21:39:55 +0300

    formatting

--- RazorEngineCore/IRazorEngineCompiledTemplateT.cs
+++ RazorEngineCore/IRazorEngineCompiledTemplateT.cs
@@ -7,15 +7,10 @@ namespace RazorEngineCore
     public interface IRazorEngineCompiledTemplate<out T> where T : IRazorEngineTemplate
     {
         void SaveToStream(Stream stream);
-        
         Task SaveToStreamAsync(Stream stream);
-        
         void SaveToFile(string fileName);
-        
         Task SaveToFileAsync(string fileName);
-        
         string Run(Action<T> initializer);
-        
         Task<string> RunAsync(Action<T> initializer);
     }
 }
--- RazorEngineCore/IRazorEngineTemplate.cs
+++ RazorEngineCore/IRazorEngineTemplate.cs
@@ -5,11 +5,13 @@ namespace RazorEngineCore
     public interface IRazorEngineTemplate
     {
         dynamic Model { get; set; }
+
         void WriteLiteral(string literal = null);
         void Write(object obj = null);
         void BeginWriteAttribute(string name, string prefix, int prefixOffset, string suffix, int suffixOffset, int attributeValuesCount);
         void WriteAttributeValue(string prefix, int prefixOffset, object value, int valueOffset, int valueLength, bool isLiteral);
         void EndWriteAttribute();
+
         Task ExecuteAsync();
         string Result();
     }
