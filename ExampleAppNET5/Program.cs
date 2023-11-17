458e6d1832724a26eed7b330b40e2b48d9131a47
Alexander Selishchev <adoconnection@gmail.com>
2023-11-17 18:11:39 +0300

    example app update with debugging example

--- ExampleAppNET5/Program.cs
+++ ExampleAppNET5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using RazorEngineCore;
@@ -9,45 +10,45 @@ namespace ExampleAppNET5
 
     class Program
     {
-        static string Content = @"
-Hello @Model.Name
-
-@foreach(var item in @Model.Items)
-{
-    <div>- @item</div>
-}
-
-<div data-name=""@Model.Name""></div>
-
-<area>
-    @{ RecursionTest(3); }
-</area>
-
-@{
-	void RecursionTest(int level){
-		if (level <= 0)
-		{
-			return;
-		}
-			
-		<div>LEVEL: @level</div>
-		@{ RecursionTest(level - 1); }
-	}
-}";
-
         static void Main(string[] args)
         {
             IRazorEngine razorEngine = new RazorEngine();
-            IRazorEngineCompiledTemplate template = razorEngine.Compile(Content);
+            IRazorEngineCompiledTemplate template1 = razorEngine.Compile("Hello <h1>@Model.Name</h1>");
 
-            string result = template.Run(new
+            string result = template1.Run(new
             {
-                    Name = "Alexander",
-                    Items = new List<string>()
-                    {
-                            "item 1",
-                            "item 2"
-                    }
+	            Name = "<b>Alex</b>"
+			});
+
+            Console.WriteLine(result);
+        }
+
+
+        static void ReadFromFileAndRun()
+        {
+            IRazorEngine razorEngine = new RazorEngine();
+            string templateText = File.ReadAllText("template2.cshtml");
+
+            IRazorEngineCompiledTemplate template2 = razorEngine.Compile(templateText, builder =>
+            {
+                builder.IncludeDebuggingInfo();
+            });
+
+            if (!Directory.Exists("Temp"))
+            {
+                Directory.CreateDirectory("Temp");
+            }
+
+            template2.EnableDebugging("Temp");
+
+            string result = template2.Run(new
+            {
+                Name = "Alexander",
+                Items = new List<string>()
+                {
+                    "item 1",
+                    "item 2"
+                }
             });
 
             Console.WriteLine(result);
